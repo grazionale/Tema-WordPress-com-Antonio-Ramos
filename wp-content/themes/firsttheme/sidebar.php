6b7877ac931e26b41e180c0fd89f31ceb7ebee29
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2017-12-19 20:07:35 -0200

    Aula 13 - post recentes - aprox 14:40 video

--- wp-content/themes/firsttheme/sidebar.php
+++ wp-content/themes/firsttheme/sidebar.php
@@ -1,7 +1,30 @@
 <div id="sidebar">
 
     <div id="sidebar-vistos">
-        Mais Vistos
+        <div id="title-vistos"><span>POSTS MAIS VISTOS</span></div>
+        
+        <ul>
+            <li>
+                <span class="vistos-number">1</span>
+                <a href="#">Quisque tincidunt, libero eu varius auctor, risus odio</a>
+            </li>
+            <li>
+                <span class="vistos-number">2</span>
+                <a href="#">suscipit turpis commodo non. Donec hendrerit massa</a>
+            </li>
+            <li>
+                <span class="vistos-number">3</span>
+                <a href="#">aliquam mi vitae elementum. In accumsan tempor laoreet.</a>
+            </li>
+            <li>
+                <span class="vistos-number">4</span>
+                <a href="#">lobortis sed et dolor. Cras fringilla erat pulvinar nunc mollis</a>
+            </li>
+            <li>
+                <span class="vistos-number">5</span>
+                <a href="#">Maecenas vel enim eget quam imperdiet consectetur.</a>
+            </li>            
+        </ul>
     </div><!-- fim sidebar-vistos -->
 
     <div id="sidebar-colunistas">
