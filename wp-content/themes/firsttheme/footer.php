d26eb27bbcc3dad7822ad2ebf13522eb1820c759
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2017-12-20 16:35:54 -0200

    Aula 14 Finalizada - Layout Pronto

--- wp-content/themes/firsttheme/footer.php
+++ wp-content/themes/firsttheme/footer.php
@@ -1,23 +1,49 @@
 <div id="footer">
 
     <div id="footer-content">
+        <div id="title-footer"><span><img src="<?php bloginfo('template_url');?>/images/logoB.png" alt="" title=""/></span></div>
 
         <div id="footer-sobre">
-            Sobre nós
+            <div id="title-sobre"><span>SOBRE NÓS</span></div>
+            
+            <p>Proin viverra bibendum erat, in lacinia ligula accumsan sed. Maecenas at magna libero. Nam non leo quis tellus vulputate fringilla. Maecenas eros ex, pharetra sit amet porttitor eu, maximus vitae metus.</p>
+            <p>Proin viverra bibendum erat, in lacinia ligula accumsan sed. Maecenas at magna libero. Nam non leo quis tellus vulputate fringilla. Maecenas eros ex, pharetra sit amet porttitor eu, maximus vitae metus.</p>
+            
         </div><!-- fim footer-sobre -->
 
         <div id="footer-paginas">
-            Páginas
+            <div id="title-paginas"><span>PÁGINAS</span></div>
+            
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Sobre nós</a></li>
+                <li><a href="#">Arquivos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
         </div><!-- fim footer-paginas -->
 
         <div id="footer-recentes">
-            Posts recentes
+            
+            <div id="title-recentes"><span>POSTS RECENTES</span></div>
+            
+            <ul>
+                <li><a href="#">Duis mattis et augue quis fringilla quaht.</a></li>
+                <li><a href="#">Vestibulum eu neque vestibulum, ultrices.</a></li>
+                <li><a href="#">Quisque pharetra eros nisi, in maximus.</a></li>
+            </ul>
+            
         </div><!-- fim footer-recentes -->
 
     </div><!-- fim footer-content -->
 
     <div id="footer-info">
-        Footer info
+        
+        <div id="info-content">
+            <span>2017 Gabriel Grazionale Gomes Fernandes Todos Direitos Reservados</span>
+            <span class="info-right">Desenvolvimento: <b>Gabriel Grazionale</b></span>
+        </div>
+        
     </div><!-- fim footer-info -->
 </div><!-- fim footer -->
 
--- wp-content/themes/firsttheme/header.php
+++ wp-content/themes/firsttheme/header.php
@@ -59,7 +59,7 @@
 
         <div id="search">
             <form action="" method="POST">
-                <input type="text" name="">
+                <input type="text" name="" placeholder="Digite sua pesquisa">
                 <input type="submit" value="" class="btn-search" name="">
             </form>
         </div><!-- fim search -->
